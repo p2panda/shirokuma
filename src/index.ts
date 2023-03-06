bfa23f8d0ed702415c2830732b334ddf8f1c6658
Andreas Dzialocha <x12@adz.garden>
2023-03-06 15:17:46 +0100

    Remove custom doc strings, they don't work

--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,7 @@ import { KeyPair, initWebAssembly } from 'p2panda-js';
 
 import { Session } from './session';
 
-export {
-  Session,
-
-  /**
-   * Ed25519 key pair to sign Bamboo entries with.
-   * @example
-   * ```
-   * import { KeyPair } from 'shirokuma';
-   *
-   * const keyPair = new KeyPair();
-   * console.log(keyPair.publicKey());
-   * ```
-   */
-  KeyPair,
-
-  /**
-   * Depending on which `shirokuma` build you chose to import into your project,
-   * the WebAssembly code needs to be initialised in different ways:
-   *
-   * 1. NodeJS: No initialisation needed. You can optionally activate debug tools
-   * for better error messages in WebAssembly code by calling `initWebAssembly`.
-   *
-   * 2. UMD, CJS and ESM builds with inlined WebAssembly code running in the
-   * browser: WebAssembly needs to be decoded and initialised by calling
-   * `initWebAssembly` once before all other methods. This will also implicitly
-   * activate debug tools for better error messages in WebAssembly code.
-   *
-   * 3. CJS and ESM "slim" builds running in browser: WebAssembly needs to be
-   * initialised by providing external "p2panda.wasm" file path as an input
-   * when calling `initWebAssembly` methods. This will also implicitly activate
-   * debug tools for better error messages in WebAssembly code.
-   */
-  initWebAssembly,
-};
+export { Session, KeyPair, initWebAssembly };
 
 export type { Options } from './session';
 export type { SchemaId, NextArgs, DocumentViewId, Fields } from './types';
