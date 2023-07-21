b1d89fb7085e8df64487b084b408f72e0d5d4f9d
adz <x12@adz.garden>
2023-07-21 12:49:23 +0200

    Fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from '@jest/types';
 
 export default {
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
 } as Config.InitialOptions;
